5820916579909b2a6cc7d2d4a32451d93cfdc7ec
Ammar Ahmed <ammar69aa59@gmail.com>
2024-05-13 18:25:59 -0700

    text-showmore/showless

---
+++ app.js
@@ -0,0 +1,13 @@
+function change() {
+    if (event.target.innerHTML == 'See Less') {
+        let text = document.getElementById('text');
+        let btn = document.getElementById('bt')
+        text.innerHTML = ' Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
+        btn.innerHTML = 'See More'
+    } else {
+        let text = document.getElementById('text');
+        text.innerHTML = 'Laboriosam quibusdam accusamus veritatis optio, corporis quae amet ad voluptatum sint esse quis ipsa quisquam sit iste cumque quasi obcaecati suscipit explicabo?Laboriosam quibusdam accusamus veritatis optio, corporis quae amet ad voluptatum sint esse quis ipsa quisquam sit iste cumque quasi obcaecati suscipit explicabo?Laboriosam quibusdam accusamus veritatis optio, corporis quae amet ad voluptatum sint esse quis ipsa quisquam sit iste cumque quasi obcaecati suscipit explicabo?Laboriosam quibusdam accusamus veritatis optio, corporis quae amet ad voluptatum sint esse quis ipsa quisquam sit iste cumque quasi obcaecati suscipit explicabo?Laboriosam quibusdam accusamus veritatis optio, corporis quae amet ad voluptatum sint esse quis ipsa quisquam sit iste cumque quasi obcaecati suscipit explicabo?Laboriosam quibusdam accusamus veritatis optio, corporis quae amet ad voluptatum sint esse quis ipsa quisquam sit iste cumque quasi obcaecati suscipit explicabo?'
+        let btn = document.getElementById('bt');
+        btn.innerHTML = 'See Less';
+    }
+};
